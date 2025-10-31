fdcd6dd4f15afcea65bc4a0e2909ee3024e81dd0
Sarfaraz <Sarfraz432@gmail.com>
2025-10-31 22:04:34 +0530

    Remove Element & merge list

---
+++ leetcode/removeElement27.py
@@ -0,0 +1,24 @@
+# 27. Remove Element
+
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+# Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+# Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+# Return k.
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
+
+
+if __name__ == "__main__":
+    nums = [3,2,2,3, 5,6, 4, 9]
+    val = 3
+    sol = Solution().removeElement(nums, val)
+    print(sol, nums[:sol])
