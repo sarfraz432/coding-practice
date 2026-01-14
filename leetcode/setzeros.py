19a24fe422233307a760c5f3c62706095f766d23
Sarfaraz <Sarfraz432@gmail.com>
2026-01-14 19:05:03 +0530

    setzeros in matrix

---
+++ leetcode/setzeros.py
@@ -0,0 +1,34 @@
+from typing import List
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+        
+        first_row_zero = any(matrix[0][j] == 0 for j in range(n))
+        first_col_zero = any(matrix[i][0] == 0 for i in range(m))
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        if first_row_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+        
+        if first_col_zero:
+            for i in range(m):
+                matrix[i][0] = 0
+
+        
+        return matrix
+if __name__ == "__main__":
+    # matrix = [[1,1,1],[1,0,1],[1,1,1]]
+    matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    sol = Solution().setZeroes(matrix)
+    print(sol)
