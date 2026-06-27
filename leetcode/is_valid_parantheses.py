6b7d45b9e8d7a8874374e19eb3a1277dcbab0181
Sarfaraz <Sarfraz432@gmail.com>
2026-06-27 22:19:38 +0530

    modified is_valid, added optimized version of paranthesis checking

--- leetcode/is_valid_parantheses.py
+++ leetcode/is_valid_parantheses.py
@@ -1,5 +1,6 @@
 
 def is_valid(para_str:str) -> bool:
+    return is_valid_optimized(para_str)
     braces_stack = []
     top = -1
     try:
@@ -22,6 +23,20 @@ def is_valid(para_str:str) -> bool:
         return True
     else:
         return False
+    
+
+def is_valid_optimized(para_str:str) -> bool:
+    mapping = {")": "(", "}": "{", "]": "["}
+    stack = []
+    for cur_char in para_str:
+        if cur_char in mapping:
+            top_element = stack.pop() if stack else "S"
+            if mapping[cur_char] != top_element:
+                return False
+        else:
+            stack.append(cur_char)
+    return len(stack) == 0
+
                 
 
 print(is_valid("()"))      # Expected: True
